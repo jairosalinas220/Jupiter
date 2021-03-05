6a9807c89cb8aa70c4c2d8f79067103351dec76f
Jairo Alberto <jairoalbertosalinasprimo210@gmail.com>
2021-03-05 18:51:18 -0500

    Add mass mentions and anti invities

--- src/Events/Message/message.js
+++ src/Events/Message/message.js
@@ -1,4 +1,4 @@
-const EventClass = require('../../Utils/Class/eventsClass.js')
+const EventClass = require('../../Utils/Class/eventsClass.js');
 module.exports = class Message extends EventClass {
     constructor(client, data) {
         super(client, {
@@ -19,9 +19,27 @@ module.exports = class Message extends EventClass {
         const prefixes = [prefix, `<@${this.client.user.id}>`, `<@!${this.client.user.id}>`, 'jupiter'];
         const usedPrefix = prefixes.find((p) => msg.content.startsWith(p));
         if (!usedPrefix || msg.author.bot) return;
-        if (usedPrefix !== prefix)
+        if (usedPrefix !== prefix) {
             msg.mentions.users.delete(msg.mentions.users.first().id);
+        }
 
+
+
+
+        const userMentionRegex = new RegExp(/(?<=(<@&))(\d{17,19})(?=>)/g)
+        const userMentionMatch = msg.content.match(regexUser);
+        const userMentionsLength = userMentionsMatch.filter(id => msg.guild.members.cache.has(id)).length
+        if(userMentionsLength === 5) {
+          return msg.reply('Please do not mention several times');
+        }
+        const inviteRegex = new RegExp(/(?<=(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/).+[A-z]/g)
+        const inviteMatch = msg.content.match(inviteMatch)
+        if(inviteMatch) {
+          const invite = await msg.client.fetchInvite(invite).catch(_ => { });
+          if(invite && invite.guild.id === msg.guild.id) {
+            return msg.reply('Invitations are forbidden on this server')
+          }
+        }
         const args = msg.content.slice(usedPrefix.length).trim().split(/ +/g)
         const command = args.shift().toLowerCase()
 
